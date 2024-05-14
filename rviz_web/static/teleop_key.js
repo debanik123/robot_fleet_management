e1f17901cea3c1468baafa7716679e4372cd3c78
Dj <debanikroy92@gmail.com>
2024-05-14 12:03:23 +0900

    log remove

--- rviz_web/static/teleop_key.js
+++ rviz_web/static/teleop_key.js
@@ -6,6 +6,39 @@ let targetAngularVel = 0;
 let maxLinearVel = 0.5; // Initial maximum linear velocity
 let maxAngularVel = 1.0; // Initial maximum angular velocity
 
+const ros = new ROSLIB.Ros({
+    url: 'ws://localhost:9090'  // Adjust the URL based on your ROS2 WebSocket server configuration
+});
+
+ros.on('connection', function () {
+    console.log('Connected to ROS2');
+});
+
+ros.on('error', function (error) {
+    console.error('Error connecting to ROS2:', error);
+});
+
+ros.on('close', function () {
+    console.log('Disconnected from ROS2');
+});
+
+// Define the cmd_vel publisher
+const cmdVelPublisher = new ROSLIB.Topic({
+    ros: ros,
+    name: '/cmd_vel',  // Adjust the topic name based on your robot's configuration
+    messageType: 'geometry_msgs/Twist'
+});
+
+function sendVelocities(linearVel, angularVel) {
+    // Publish Twist message with calculated velocities
+    const twist = new ROSLIB.Message({
+        linear: { x: linearVel, y: 0, z: 0 },
+        angular: { x: 0, y: 0, z: angularVel }
+    });
+
+    cmdVelPublisher.publish(twist);
+}
+
 
 teleopButton.addEventListener('click', function() {
     // Start capturing keyboard data
@@ -15,7 +48,8 @@ teleopButton.addEventListener('click', function() {
 
 // Keyboard event handlers
 function onKeyDown(event) {
-    console.log('targetLinearVel: ', targetLinearVel, 'targetAngularVel: ', targetAngularVel);
+    // console.log('targetLinearVel: ', targetLinearVel, 'targetAngularVel: ', targetAngularVel);
+    sendVelocities(targetLinearVel, targetAngularVel);
     // Capture keyboard data and perform teleoperation
     switch(event.key) {
         case 'ArrowUp':
@@ -82,7 +116,7 @@ function onKeyDown(event) {
             break;
 
         default:
-            console.log('Pressed key:', event.key);
+            // console.log('Pressed key:', event.key);
             break;
 
         
@@ -104,6 +138,8 @@ function onKeyUp(event) {
     // Stop motion when key is released
     targetLinearVel = 0;
     targetAngularVel = 0;
+
+    sendVelocities(targetLinearVel, targetAngularVel);
 
     // Publish zero velocities
     // sendVelocities(targetLinearVel, targetAngularVel);
